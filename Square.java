fbc55eca61baf059afc22656d98c735f1071e56c
owalsh28 <69826082+owalsh28@users.noreply.github.com>
2021-01-07 10:23:20 -0600

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,8 +1,34 @@
-final int MAX_SIDE_LENGTH = 10;
+
 public class Square 
 {
   private double side;
 
 
+
+public final int MAX_SIDE_LENGTH = 10;
+public Square() {
+  side = 2.0;
+
 }
+public Square(double theSide) {
+  //if (theSide<= MAX_SIDE_LENGTH) {
+
+  
+  side = theSide;
+ // }
+ // else side = 2.0;
+}
+public void getSide() {
+  System.out.println(side);
+  
+}
+public void setSide(double theSide) { 
+  side = theSide;
+
+}
+}
+
+
+
+
 
